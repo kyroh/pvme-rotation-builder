d9587e7b83a2167dad3e6299f315ddea4faa57e1
Aaron Tarajos <aarontarajos@midpointmarkets.org>
2023-05-25 10:53:24 -0700

    updated stuff

--- standard_ability.py
+++ standard_ability.py
@@ -1,11 +1,10 @@
 #
-# Author - Aaron Tarajos
+# Author - kyroh
 # 
 # This source code is licensed under the Attribution-NonCommercial-ShareAlike 4.0 International license found in the LICENSE file in the root directory of this source tree.
 # 
 #
 
-import random
 import os
 import json
 import math
@@ -25,7 +24,7 @@ class StandardAbility:
         self.ability_input = 'db'
         self.mh_input = 'Wand of the praesul'
         self.oh_input = 'Imperium core'
-        self.th_input = 'Fractured staff of Armadyl'
+        self.th_input = 'Crystal staff'
         self.type = '2h'
         self.bonus = 12
         self.spell_input = 99
@@ -33,27 +32,31 @@ class StandardAbility:
         self.base_range_level = 99
         self.base_strength_level = 99
         self.aura_input = 'None'
-        self.potion_input = 'Elder overload'
-        self.prayer_input = 'None'
-        self.precise_rank = 6
-        self.equilibrium_rank = 2
+        self.potion_input = 'None'
+        self.prayer_input = 'Torment'
+        self.precise_rank = 0
+        self.equilibrium_rank = 0
         
         # Variables from methods
         self.boosted_levels = self.calculate_levels()
         self.boosted_magic_level = self.boosted_levels[0]
         self.boosted_range_level = self.boosted_levels[1]
         self.boosted_strength_level = self.boosted_levels[2]
+        
         self.abil_params = self.get_abil_params()
         self.style = self.abil_params[0]
         self.class_n = self.abil_params[1]
         self.type_n = self.abil_params[2]
         self.min_dmg = self.abil_params[3]
         self.max_dmg = self.abil_params[4]
+        
         self.ability_dmg = self.base_ability_dmg()
+        
         self.prayer_boost = self.prayer_dmg()
         self.magic_prayer = self.prayer_boost[0]
         self.range_prayer = self.prayer_boost[1]
         self.melee_prayer = self.prayer_boost[2]
+        
         self.aura_boost = self.aura_passive()
         self.magic_aura = self.aura_boost[0]
         self.range_aura = self.aura_boost[1]
@@ -255,11 +258,11 @@ class StandardAbility:
         fixed = 0
         
         if self.style == 'magic':
-            fixed = math.floor(self.ability_dmg * (self.min_dmg * (1 + self.magic_prayer)))
+            fixed = math.floor(math.floor(self.ability_dmg * self.min_dmg) * (1 + self.magic_prayer))
         elif self.style == 'range':
-            fixed = math.floor(self.ability_dmg * (self.min_dmg * (1 + self.range_prayer)))
+            fixed = math.floor(math.floor(self.ability_dmg * self.min_dmg) * (1 + self.range_prayer))
         elif self.style == 'melee':
-            fixed = math.floor(self.ability_dmg * (self.min_dmg * (1 + self.melee_prayer)))
+            fixed = math.floor(math.floor(self.ability_dmg * self.min_dmg) * (1 + self.melee_prayer))
         else:
             pass
         return fixed
@@ -267,11 +270,11 @@ class StandardAbility:
     # Computes variable dmg with prayer modifier
     def variable(self):
         if self.style == 'magic':
-            variable = math.floor(self.ability_dmg * ((self.max_dmg - self.min_dmg) * (1 + self.magic_prayer)))
+            variable = math.floor(math.floor(self.ability_dmg * (self.max_dmg - self.min_dmg)) * (1 + self.magic_prayer))
         elif self.style == 'range':
-            variable = math.floor(self.ability_dmg * ((self.max_dmg - self.min_dmg) * (1 + self.range_prayer)))
+            variable = math.floor(math.floor(self.ability_dmg * (self.max_dmg - self.min_dmg)) * (1 + self.range_prayer))
         elif self.style == 'melee':
-            variable = math.floor(self.ability_dmg * ((self.max_dmg - self.min_dmg) * (1 + self.melee_prayer)))
+            variable = math.floor(math.floor(self.ability_dmg * (self.max_dmg - self.min_dmg)) * (1 + self.melee_prayer))
         else:
             pass
         return variable
@@ -282,7 +285,7 @@ class StandardAbility:
         dmg_values = self.dpl()
         fixed = dmg_values[0]
         variable = dmg_values[1]
-        precise = 0.015 * (fixed + variable) * rank
+        precise = math.floor(0.015 * (fixed + variable) * rank)
         p_f = math.floor(fixed + precise)
         p_v = math.floor(variable - precise)
         return [p_f, p_v]
@@ -363,12 +366,11 @@ class StandardAbility:
     
     # Computes the dmg boost from ultimates and specs
     def sunshine(self):
+        dmg_values = self.equilibrium
+        fixed = dmg_values[0]
+        variable = dmg_values[1]
         pass
-    
+
 test = StandardAbility()
-
-dmg = test.equilibrium()
-
-print(dmg)
 
 
