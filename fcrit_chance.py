058828cf97cc02aa95d09fc8b9ce1c86e4767b50
ocditia <96238651+ocditia@users.noreply.github.com>
2023-06-03 22:14:42 +0200

    Create fcrit_chance.py
    does the maths for forced crit chance

---
+++ fcrit_chance.py
@@ -0,0 +1,63 @@
+class fcrit:
+    def __init__(self):
+        self.style = 'Melee' #combat style of the player
+        self.grim = 1 #1 if grim is active
+        self.biting = 3 #biting level of the player
+        self.bitingarmour = 1 # 1 if level 20 armour
+        self.kalg = 1 #1 if player uses a kalg familiar
+        self.critikal = 1 #1 if kalg scroll is active
+        self.corbicula = 1 #number of pens with elder corbiculas
+        self.abil = "Meteor Strike" #which abil is used
+        self.warpriest = 0 #number of pieces
+        self.fury = 2 # number of fury/conc hits, 2 for gfury
+        self.gfury =  0 #1 if guaranteed crit
+        self.reaver = 1 # 0 or 1
+        self.channelers = 0 # 0 or 1
+        self.channels = 0 #number hit it is in the channel
+        self.champions = 0 # 0, 1 or 2
+        self.bleeds = 0 #number of bleeds on the boss (for champ ring)
+        self.stalkers = 0 # 0, 1 or 2
+        self.bow = 1 #check if a bow is equipped for stalker's ring
+        self.deathspore = 0 #1 if deathspore arrows are equipped
+
+
+    def calc_fcrit(self):
+        if self.gfury == 1:
+            return 1.00
+
+        fcrit_chance = 0
+        if self.warpriest > 0:
+            fcrit_chance += self.warpriest
+        else:
+            fcrit_chance += (self.biting * 0.02) * (1+self.bitingarmour * 0.1)
+
+        fcrit_chance = (fcrit_chance + self.kalg * 0.05 + self.critikal * 0.01
+                        + self.reaver * 0.05 + self.grim * 0.12)
+
+        if self.style == 'Melee':
+            fcrit_chance += self.fury * 0.05
+            if self.champions == 1:
+                fcrit_chance += 0.03
+            elif self.champions == 2:
+                fcrit_chance += 0.03 + 0.01 * self.bleeds
+            if self.abil == 'Meteor Strike':
+                fcrit_chance += self.corbicula * 0.2
+
+        elif self.style == 'Magic':
+            fcrit_chance += self.fury * 0.05 + self.channelers * self.channels * 0.04
+
+        elif self.style == 'Ranged':
+            fcrit_chance += self.deathspore * 0.03
+            if self.stalkers == 1:
+                fcrit_chance += 0.03
+            elif self.stalkers == 2:
+                fcrit_chance += 0.03 + 0.01 * self.bow
+
+        elif self.style == 'Necromancy': #placeholder
+            return "you're lying this skill isn't out yet"
+
+        return round(fcrit_chance,3)
+            
+test = fcrit()
+crit_chance = test.calc_fcrit()
+print(crit_chance)
