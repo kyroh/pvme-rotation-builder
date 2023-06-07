dff0feb71ce8c785f9954a460067a5b7e300a166
Aaron Tarajos <aarontarajos@midpointmarkets.org>
2023-06-07 15:36:10 -0700

    testing UI stuff

--- ui/test_ui.py
+++ ui/test_ui.py
@@ -1,3 +1,25 @@
 from st_on_hover_tabs import on_hover_tabs
 import streamlit as st
+import json
+import os
+
+with open(os.path.join('utils', 'GEAR.json'), 'r') as g:
+            gear = json.load(g)
+
+
+helms = [item['name'] for item in gear if item['slot'] == 'helm']
+
+with st.form("main_inputs"):
+    st.write("Inside the form")
+    helm = st.selectbox('Select and option', helms)
+    
+    submitted = st.form_submit_button("Submit")
+    
+    if submitted:
+        st.write("helm", helm)
+
+st.write("Outside the form")
+    
+    
+
 
