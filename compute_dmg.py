a814b511e737e87b9ba62b779bcf089e49b97e60
Aaron Tarajos <aarontarajos@midpointmarkets.org>
2023-06-07 13:07:57 -0700

    idk

--- compute_dmg.py
+++ compute_dmg.py
@@ -40,7 +40,7 @@ class Inputs:
         self.precise_rank = 6
         self.equilibrium_rank = 0
         self.lunging_rank = 0
-        self.dmg_output = 'MAX'
+        self.dmg_output = 'MIN'
         self.abil_params = self.get_abil_params()
         self.name = self.abil_params[0]
         self.fixed_dmg = self.abil_params[1]
