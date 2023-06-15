156bad754546baa0c64f943c72b91c713260b1a7
ocditia <96238651+ocditia@users.noreply.github.com>
2023-06-15 14:08:21 +0200

    Create var_only_buffs.py
    order for the buffs is unknown - some can be tested but not all

---
+++ components/var_only_buffs.py
@@ -0,0 +1,25 @@
+class varOnlyBoosts:
+    def __init__(self,damage):
+        self.revenge = 0 #num of stacks
+        self.pernix_quiver = 0 #equipped yes/no - check if it works off style
+        self.spendthrift = 0 #rank
+        self.slayer_helm = 0 #tier 0-5
+        self.bane = 0 #yes no - only og bane, not jas bane
+        self.type = "ability" #ability or auto
+
+
+    def calc_var_boost(self):
+        if self.slayer_helm > 0:
+            slayerHelm = 0.12 + 0.5 * self.slayer_helm
+        else:
+            continue
+
+        if self.type == "ability":
+            bane = 0.25
+        elif self.type == "auto":
+            bane = 0.4
+        else:
+            bane = 0
+            
+        return int(damage * (1 + self.revenge 8 0.1 + self.pernix_quiver * 0.04
+                             + slayerHelm + bane + self.spendthrift**2/100))
