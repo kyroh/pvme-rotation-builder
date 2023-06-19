dcf6bff078df91d37dcc003aa20a916e19ca89ae
Aaron Tarajos <aarontarajos@midpointmarkets.org>
2023-06-19 13:05:18 -0700

    starting on active buffs

---
+++ components/active_buffs.py
@@ -0,0 +1,22 @@
+from components.inputs import UserInputs
+
+class ActiveBuffs:
+    def __init__(self, ability, weapon):
+        self.inputs = UserInputs(ability, weapon)
+        
+    def exsang(self):
+        pass
+    
+    def incite(self):
+        pass
+    
+    def gconc(self):
+        pass
+    
+    def channelers(self):
+        pass
+    
+    def ruby_aurora(self):
+        pass
+    
+    
